fa55d253d3b9a4f4b85da60aee4f3c52d2a94f10
Mateusz Wojdyła <152260523+Mateusz-Wojdyla@users.noreply.github.com>
2024-02-24 22:41:46 +0100

    2

--- code/app.js
+++ code/app.js
@@ -1,1 +1,28 @@
-console.log("hello")
+let isOpen = false;
+
+const hamburger = document.getElementById("hamburger");
+const hamburgerMenu = document.querySelector(".hamburger-slide-menu")
+
+function renderHamburger(){
+	hamburger.className = isOpen ? "open":"close";
+    
+	
+}
+function toggleHamburger(){
+    
+	isOpen = !isOpen
+	renderHamburger()
+    
+}
+
+
+hamburger.addEventListener("click",function(){
+    toggleHamburger();
+   if(hamburgerMenu.style.top === "-80em" || hamburgerMenu.style.top === ""){
+        hamburgerMenu.style.top = "8em"
+   }else{
+    hamburgerMenu.style.top = "-80em"
+   }
+	
+
+});
