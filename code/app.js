74ec26bb0a74f263ac639c518080a8652da0bcfc
Mateusz Wojdyła <152260523+Mateusz-Wojdyla@users.noreply.github.com>
2024-02-26 11:29:28 +0100

    3

--- code/app.js
+++ code/app.js
@@ -2,6 +2,9 @@ let isOpen = false;
 
 const hamburger = document.getElementById("hamburger");
 const hamburgerMenu = document.querySelector(".hamburger-slide-menu")
+const login = document.querySelector(".login-container")
+const slideRegister = document.querySelector("#slide-button-register")
+const registerExit = document.querySelector(".exit")
 
 function renderHamburger(){
 	hamburger.className = isOpen ? "open":"close";
@@ -26,3 +29,14 @@ hamburger.addEventListener("click",function(){
 	
 
 });
+
+
+slideRegister.addEventListener("click",function(){
+     if(login.style.top === "-50em" || login.style.top === ""){
+          login.style.top = "10em"
+     }
+})
+
+registerExit.addEventListener("click",function(){
+     login.style.top = "-50em"
+})
