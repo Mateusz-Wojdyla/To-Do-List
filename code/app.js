0f9b8e331ab8c7630c33ae143d871562cb0279b8
Mateusz Wojdyła <152260523+Mateusz-Wojdyla@users.noreply.github.com>
2024-02-26 15:37:29 +0100

    26.02

--- code/app.js
+++ code/app.js
@@ -1,24 +1,27 @@
 let isOpen = false;
 
 const hamburger = document.getElementById("hamburger");
-const hamburgerMenu = document.querySelector(".hamburger-slide-menu")
-const login = document.querySelector(".login-container")
-const slideRegister = document.querySelector("#slide-button-register")
-const registerExit = document.querySelector(".exit")
+const hamburgerMenu = document.querySelector(".hamburger-slide-menu");
+
+const buttonTryIt = document.querySelector(".button-64")
+
+const register = document.querySelector(".register-container");
+const slideRegister = document.querySelector("#slide-button-register");
+const registerExit = document.querySelector(".exit");
+const registerLogin = document.querySelector("#btn-login");
+
+const loginExit = document.querySelector(".login_exit");
+const menuLoginBtn = document.querySelector("#slide-button-login");
+const loginContainer = document.querySelector(".login-container");
+const newRegister = document.querySelector("#login-container-register")
 
 function renderHamburger(){
-	hamburger.className = isOpen ? "open":"close";
-    
-	
+	hamburger.className = isOpen ? "open":"close";	
 }
-function toggleHamburger(){
-    
+function toggleHamburger(){  
 	isOpen = !isOpen
-	renderHamburger()
-    
+	renderHamburger()   
 }
-
-
 hamburger.addEventListener("click",function(){
     toggleHamburger();
    if(hamburgerMenu.style.top === "-80em" || hamburgerMenu.style.top === ""){
@@ -26,17 +29,44 @@ hamburger.addEventListener("click",function(){
    }else{
     hamburgerMenu.style.top = "-80em"
    }
-	
+});
+slideRegister.addEventListener("click",function(showRegister){
+     if(register.style.top === "-50em" || register.style.top === ""){
+          register.style.top = "10em"
+     }
+     if(loginContainer.style.top = "10em"){
+          loginContainer.style.top = "-50em"
+          register.style.top = "10em"
 
+     }
+});
+registerExit.addEventListener("click",function(){
+     register.style.top = "-50em"
 });
 
-
-slideRegister.addEventListener("click",function(){
-     if(login.style.top === "-50em" || login.style.top === ""){
-          login.style.top = "10em"
+menuLoginBtn.addEventListener("click",function(){
+     console.log("click")
+     if(loginContainer.style.top === "-50em" || loginContainer.style.top === ""){
+          loginContainer.style.top = "10em"
      }
-})
 
-registerExit.addEventListener("click",function(){
-     login.style.top = "-50em"
+     if( register.style.top === "10em"){
+          register.style.top = "-50em"
+          loginContainer.style.top = "10em"
+     }
+
+});
+loginExit.addEventListener("click",function(){
+     loginContainer.style.top = "-50em"
+});
+registerLogin.addEventListener("click",function(){
+     register.style.top = "-50em"
+     loginContainer.style.top = "10em"
+})
+newRegister.addEventListener("click",function(){
+     loginContainer.style.top = "-50em"
+     register.style.top = "10em"
+})
+buttonTryIt.addEventListener("click",function(){
+     register.style.top = "10em"
 })
