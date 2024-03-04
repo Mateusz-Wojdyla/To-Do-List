a6d14521d4522e67c79f5e1242e3673aae350584
Mateusz Wojdyła <152260523+Mateusz-Wojdyla@users.noreply.github.com>
2024-03-04 20:16:39 +0100

    04.03

--- code/app.js
+++ code/app.js
@@ -1,6 +1,81 @@
 const logOut = document.querySelector("#btn-logOut");
+const taskWindow = document.querySelector(".task-add-window");
+const calendaryNav = document.querySelector(".calendaryChose");
+const addTask = document.querySelector(".addTask-nav");
+const exitBtn = document.querySelector(".exit-btn");
+////////  calendar days///////
+const pon = document.querySelector(".pon");
+const wt = document.querySelector(".wt");
+const sr = document.querySelector(".sr");
+const czw = document.querySelector(".czw");
+const pt = document.querySelector(".pt");
+const so = document.querySelector(".so");
+const ndz = document.querySelector(".ndz");
+//////////////////////////////////////////////////////
+const poniedzialek = document.querySelector("#poniedzialek");
+const wtorek = document.querySelector("#wtorek");
+const sroda = document.querySelector("#sroda");
+const czwartek = document.querySelector("#czwartek");
+const piatek = document.querySelector("#piatek");
+const sobota = document.querySelector("#sobota");
+const niedziela = document.querySelector("#niedziela");
+////////// TASK WINDOW ////////////
+exitBtn.addEventListener("click", function(){
+    taskWindow.style.display = "none"
+})
+addTask.addEventListener("click",function(){
+    taskWindow.style.display = "flex"
+})
 
 
-logOut.addEventListener("click", function(){
-    
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // Funkcja do dodawania boxa
+//  function dodajBox(rubryka) {
+//     var box = document.createElement("div");
+//     box.className = "box";
+//     box.textContent = "Nowy box";
+//     document.querySelector("." + rubryka).appendChild(box);
+// }
+
+// // Funkcja do usuwania boxa
+// function usunBox(rubryka) {
+//     var boxes = document.querySelector("." + rubryka).querySelectorAll(".box");
+//     if (boxes.length > 0) {
+//         var lastBox = boxes[boxes.length - 1];
+//         lastBox.parentNode.removeChild(lastBox);
+//     }
+// }
+
+// // Przykładowe użycie
+// dodajBox("pon");
+// dodajBox("pon");
+// usunBox("pon");
