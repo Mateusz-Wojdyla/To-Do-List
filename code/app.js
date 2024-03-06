e433fd6d8a62bc268b595bdc504f07ce2e3f242f
Mateusz Wojdyła <152260523+Mateusz-Wojdyla@users.noreply.github.com>
2024-03-06 17:04:58 +0100

    2

--- code/app.js
+++ code/app.js
@@ -12,6 +12,11 @@ const pt = document.querySelector(".pt");
 const so = document.querySelector(".so");
 const ndz = document.querySelector(".ndz");
 //////////////////////////////////////////////////////
+const dayExit = document.querySelector("#exit-daySelect");
+const selectDay = document.querySelector(".selectDay");
+const calendarTaskAdd = document.querySelector("#calendar-select");
+
+///////////////SElect DAY WINDOW/////////
 const poniedzialek = document.querySelector("#poniedzialek");
 const wtorek = document.querySelector("#wtorek");
 const sroda = document.querySelector("#sroda");
@@ -20,45 +25,40 @@ const piatek = document.querySelector("#piatek");
 const sobota = document.querySelector("#sobota");
 const niedziela = document.querySelector("#niedziela");
 ////////// TASK WINDOW ////////////
-exitBtn.addEventListener("click", function(){
-    taskWindow.style.display = "none"
-})
-addTask.addEventListener("click",function(){
-    taskWindow.style.display = "flex"
-})
+exitBtn.addEventListener("click", function () {
+	taskWindow.style.display = "none";
+});
+addTask.addEventListener("click", function () {
+	taskWindow.style.display = "flex";
+});
+dayExit.addEventListener("click", function () {
+	selectDay.style.display = "none";
+});
+calendarTaskAdd.addEventListener("click", function () {
+	selectDay.style.display = "flex";
+});
+
+////////////////////ADD TASK FOR DAYS /////////////
+const daysSelect = document.querySelectorAll(
+	".selectDay .days-section .days-select"
+);
+
+document.addEventListener("DOMContentLoaded", function () {
+	daysSelect.forEach(function (day) {
+		day.addEventListener("click", function () {
+			console.log("click");
+			if (day.style.border === "none" || day.style.border === "") {
+				day.style.border = "solid 2px white";
+			} else {
+				day.style.border = "none";
+			}
+		});
+	});
+});
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- // Funkcja do dodawania boxa
+// Funkcja do dodawania boxa
 //  function dodajBox(rubryka) {
 //     var box = document.createElement("div");
 //     box.className = "box";
